6d553b3a41c3d8a220001f904bd3ec465f325f21
men <mengistuabebe06@gmail,.com>
2022-04-19 13:20:19 +0300

    product

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -57,6 +57,9 @@
 														<a title="Categories" href="{{route('admin.category')}}">Categories</a>
 													</li>
 													<li class="menu-item">
+														<a title="products" href="{{route('admin.category')}}">Categories</a>
+													</li>
+													<li class="menu-item">
 															<a href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
 														</li>
 													<form id="logout-form" method="POST" action="{{route('logout')}}"> 
